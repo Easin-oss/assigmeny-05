e7e14d65a06317a7f4482ef45a568ebcee3eb513
Your Name <you@example.com>
2025-08-29 21:31:37 +0600

    js update

--- script/emergency.js
+++ script/emergency.js
@@ -16,7 +16,27 @@ const cheackover = [
 ]
 
 const cardContainer = document.getElementById("card-container")
-cheackover.forEach(service=> {
+cheackover.forEach(service => {
     const card = document.createElement("div")
-    card.className = "border-2 border-red-500 bg-white w-[430px] h-[360px] rounded-2xl p-8 shadow-lg"
+    card.className = "border-2 border-red-500 bg-white w-[430px] h-[360px] rounded-2xl p-8 shadow-lg";
+
+
+    card.innerHTML = `
+    <div class="flex justify-between items-center mb-5">
+                <img src="./assets/${service.icon}" alt="" class="w-[60px] h-[60px] bg-[#ffe3e2] rounded-md">
+                <button class="btn btn-circle" onclick="increaseHeart()">
+                    <i class="fa-solid fa-heart"></i>
+                </button>
+            </div>
+            <div class="text-center mb-5">
+                <h1 class="text-2xl font-bold mb-3">${service.name}</h1>
+                <p class="text-[20px] mb-3">${service.type}</p>
+                <h1 class="text-3xl font-bold mb-3">${service.number}</h1>
+                <p class="bg-[#f2f2f2] w-[61px] h-[40px] text-center rounded-2xl px-4 py-2 mx-auto">${service.tag}</p>
+            </div>
+            
+
+
+    `
 })
+
